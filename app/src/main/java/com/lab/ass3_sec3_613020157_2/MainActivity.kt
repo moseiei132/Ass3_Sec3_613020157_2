ac889041ffca038fb2164a5ec945dff05cec0c7e
moseiei132 <moseiei132@gmail.com>
2020-08-17 16:12:17 +0700

    Assignment 3

---
+++ app/src/main/java/com/lab/ass3_sec3_613020157_2/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.lab.ass3_sec3_613020157_2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
